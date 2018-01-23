bcdee1066da286ae154ced9a9ab574b360017034
Soleks <SolekX@gmail.com>
2018-01-23 23:38:02 +0300

    Lesson_2\StarGame: cosmetic fix

--- Lesson_2/StarGame/StarGame/Form1.Designer.cs
+++ Lesson_2/StarGame/StarGame/Form1.Designer.cs
@@ -101,7 +101,7 @@ namespace StarGame
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MainMenuStrip = this.menuStrip1;
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Star Game";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
             this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
